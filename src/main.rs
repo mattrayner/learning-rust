3f64c81f5dd0d2383556b0080de0905f44f3124a
Matt Rayner <m@rayner.io>
2018-02-03 23:28:48 +0000

    Operators

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,40 @@
 use std::mem;
 
+fn operators() {
+  // arithmetic
+  let mut a = 2+3*4; // +-*/
+  println!("{}", a);
+  a = a+1; // -- and ++ are not supported in Rust
+  println!("{}", a);
+  a -= 2; // a = a - 2
+          // -= += /= %=
+
+  println!("remainder of {} / {} = {}", a, 3, (a%3));
+
+  let a_cubed = i32::pow(a, 3);
+  println!("{} cubed = {}", a, a_cubed);
+
+  let b = 2.5;
+  let b_cubed = f64::powi(b, 3);
+  let b_to_power_of_pi = f64::powf(b, std::f64::consts::PI);
+  println!("{} cubed = {}, {}^pi = {}", b, b_cubed, b, b_to_power_of_pi);
+
+  // bitwise
+  let c = 1 | 2; // | OR & AND ^ XOR ! NOR
+                 // 01 OR 10 == 11 == 3_10
+  println!("1|2 = {}", c);
+  let two_to_10 = 1 << 10;
+  println!("2^10 = {}", two_to_10);
+
+  // logical
+  let pi_less_4 = std::f64::consts::PI < 4.0; // true
+  println!("pi<4? {}", pi_less_4);
+  // > <= >= ==
+  let x = 5;
+  let x_is_5 = x == 5; //true
+  println!("{} = 5? {}", x, x_is_5);
+}
+
 fn main() {
   // unsigned 0+
   let a:u8 = 123; // 8bits
@@ -36,4 +71,7 @@ fn main() {
 
   let g = 4>0;
   println!("g = {}, size = {} bytes", g, mem::size_of_val(&g));
+
+  println!("\n\r--- OPERATORS ---");
+  operators();
 }
